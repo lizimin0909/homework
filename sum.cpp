77aac4b07cfc4e49c76e14be860d944c5065c634
lizimin0909 <72572098+lizimin0909@users.noreply.github.com>
2020-12-22 18:23:18 +0800

    Add files via upload

---
+++ sum.cpp
@@ -0,0 +1,32 @@
+﻿// sum.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	int n, x, m = 0;
+	cout << "Input x:";
+	cin >> x;
+	cout << "Input n:";
+	cin >> n;
+	int k = 0;
+	int a = 0;
+	double S = 0, e = 1e-6;
+	do
+	{
+		double l = 1, m = 1;
+		for (int i = 1; i <= 2 * k + 1; ++i)
+		{
+			l *= x;
+			m *= i;
+		}
+		double sum = l / m;
+		if (k % 2 != 0) sum *= -1;
+		S += sum;
+		k++;
+		a++;
+	} while (k <= n && (S >= e || S <= -e));
+	cout << "Sum = " << S << endl;
+	cout << "Number of addends = " << a << endl;
+}
